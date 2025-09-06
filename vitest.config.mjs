54342cb3df8009971055cd709cea64588ab868d7
ahts4962 <181621037+ahts4962@users.noreply.github.com>
2025-09-06 22:18:50 +0900

    Add vitest for testing

---
+++ vitest.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from "vitest/config";
+import { resolve } from "path";
+
+export default defineConfig({
+	resolve: {
+		alias: {
+			obsidian: resolve(__dirname, "test/mock/obsidian.ts"),
+		},
+	},
+});
